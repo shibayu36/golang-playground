e45a22b0324c4550d6e4317af35f116d1fe551ed
shibayu36 <shibayu36@gmail.com>
2017-09-22 14:00:57 +0900

    append

--- append.go
+++ append.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	var s []int
-//	printSlice(s)
+	//	printSlice(s)
 
 	// append works on nil slices.
 	s = append(s, 0)
@@ -13,22 +13,22 @@ func main() {
 	// The slice grows as needed.
 	s = append(s, 1)
 	printSlice(s)
-	
+
 	s = append(s, 2)
 	printSlice(s)
-	
+
 	// We can add more than one element at a time.
 	s = append(s, 2, 3, 4)
 	printSlice(s)
-	
+
 	s = s[:4]
 	printSlice(s)
 	s = s[:8]
 	printSlice(s)
-	
+
 	s = s[2:]
 	printSlice(s)
-	
+
 	s = append(s, 1)
 	printSlice(s)
 }
