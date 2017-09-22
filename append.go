0d2152bf16450bc57685f1b30c7ff73b45f58e04
shibayu36 <shibayu36@gmail.com>
2017-09-22 13:10:16 +0900

    slice sample

---
+++ append.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+//	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+	
+	s = append(s, 2)
+	printSlice(s)
+	
+	// We can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+	
+	s = s[:4]
+	printSlice(s)
+	s = s[:8]
+	printSlice(s)
+	
+	s = s[2:]
+	printSlice(s)
+	
+	s = append(s, 1)
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("mem=%p len=%d cap=%d %v\n", &s[0], len(s), cap(s), s)
+}
