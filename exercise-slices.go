ee02d4998c0c8cfaa24f75251e90bb45925154c3
shibayu36 <shibayu36@gmail.com>
2017-09-22 16:16:16 +0900

    rangeを使って書き直し

--- exercise-slices.go
+++ exercise-slices.go
@@ -5,9 +5,9 @@ import "golang.org/x/tour/pic"
 func Pic(dx, dy int) [][]uint8 {
 	pic := make([][]uint8, dy)
 
-	for y := 0; y < dy; y++ {
+	for y := range pic {
 		pic[y] = make([]uint8, dx)
-		for x := 0; x < dx; x++ {
+		for x := range pic[y] {
 			pic[y][x] = uint8((x + y) / 2)
 		}
 	}
